2f502160cf776bebb82a3eb3514f2031349db75b
Andy <studyoffficial@gmail.com>
2024-02-12 14:59:27 +0300

    fix 2

--- clickersuid/sub.py
+++ clickersuid/sub.py
@@ -10,45 +10,80 @@ path = r'C:\Users\Andy\PycharmProjects\suidproject\png/'
 
 requestnum = 0
 
+def open_outlook():
+    xoutlook, youtlook = pyautogui.locateCenterOnScreen(path + 'outlook.PNG', confidence=0.85)
+    pyautogui.click(xoutlook, youtlook)
+    youtlook -= 30
+    pyautogui.click(xoutlook, youtlook)
+    sleep(0.2)
+    print('Outlook открыт')
+
+def open_browser():
+    x, y = pyautogui.locateCenterOnScreen(path + 'yndx.PNG', confidence=0.8)
+    pyautogui.click(x, y)
+
+def open_suid():
+    x, y = pyautogui.locateCenterOnScreen(path + 'suid.PNG', confidence=0.8)
+    pyautogui.click(x, y)
+
+def open_sudir():
+    try:
+        x, y = pyautogui.locateCenterOnScreen(path + 'sudir.PNG', confidence=0.95)
+        pyautogui.click(x, y)
+    except pyautogui.ImageNotFoundException:
+        pass
+
+def copymail():
+    n, m = pyautogui.locateCenterOnScreen(path + 'copymail.PNG', confidence=0.95)
+    pyautogui.click(n, m)
+
+def copyusername():
+    xset, yset = pyautogui.locateCenterOnScreen(path + 'delete.PNG', confidence=0.95)
+    xset += 90
+    pyautogui.moveTo(xset, yset)
+    pyautogui.rightClick()
+    n, m = pyautogui.locateCenterOnScreen(path + 'copymail.PNG', confidence=0.95)
+    pyautogui.click(n, m)
+
+def copyrolename():
+    n, m = pyautogui.locateCenterOnScreen(path + 'rolename.PNG', confidence=0.7)
+    m += 160
+    n += 50
+    pyautogui.tripleClick(n, m)
+    pyautogui.rightClick(n, m)
+    n, m = pyautogui.locateCenterOnScreen(path + 'copymail.PNG', confidence=0.95)
+    pyautogui.click(n, m)
+
+def request_suid():
+    n, m = pyautogui.locateCenterOnScreen(path + 'zayavki.PNG', confidence=0.95)
+    pyautogui.click(n, m)
+    m += 25
+    pyautogui.click(n, m)
+
+
+
+
+
 # Цикл
 
 while requestnum < requests:
     # Обнуляем значения
     n, m, x, y, across, bcross, ax, ay, bx, by, xset, yset, xoutlook, youtlook = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
-    # open browser
-    pyautogui.click(122, 1060)
-    # open suid
-    pyautogui.click(44, 85)
+    open_browser()
+    open_suid()
+    open_outlook()
 
-    # ждем загрузки SUID и откр письмо
-
-    # open outlook
-    xoutlook, youtlook = pyautogui.locateCenterOnScreen(path + 'outlook.PNG', confidence=0.85)
-    pyautogui.click(xoutlook, youtlook)
-    youtlook -= 30
-    pyautogui.click(xoutlook, youtlook)
-    sleep(0.5)
-
-    # copy username from mail
-    xset, yset = pyautogui.locateCenterOnScreen(path + 'delete.PNG', confidence=0.95)
-    xset += 90
-    pyautogui.moveTo(xset, yset)
-    pyautogui.rightClick()
-    # copy
-    n, m = pyautogui.locateCenterOnScreen(path + 'copymail.PNG', confidence=0.95)
-    pyautogui.click(n, m)
+    copyusername()
 
     # open browser
-    pyautogui.click(122, 1060)
-    sleep(8)
-    pyautogui.click(192, 189)
-    sleep(1)
-    pyautogui.click(192, 209)
-    sleep(2)
+    open_browser()
+    sleep(5)
+    request_suid()
+
     pyautogui.click(554, 311)
     sleep(3)
-    # Ждем загр стр запроса
+
 
     pyautogui.click(207, 455)
     # paste
@@ -57,9 +92,8 @@ while requestnum < requests:
     pyautogui.click(239, 605)
     pyautogui.click(40, 482)
     # загрузка польз
-    # Название ресурса открыть Outlook
-    pyautogui.click(76, 1056)
-    pyautogui.click(65, 1032)
+
+    open_outlook()
 
 
 
